c9f267d9e436765691a802c4ba1228bb3b933d5a
Newton Borges de Gouveia Junior <108380314+NewtonJunior1@users.noreply.github.com>
2023-10-21 16:52:37 -0300

    Update exer_3.java

--- exer_3.java
+++ exer_3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Main {
+public class exer_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String mes[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio",
