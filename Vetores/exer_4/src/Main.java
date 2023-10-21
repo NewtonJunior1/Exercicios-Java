ba0bde68328f15cc760c4e7f2968d98140d423aa
Newton Borges de Gouveia Junior <108380314+NewtonJunior1@users.noreply.github.com>
2023-10-21 17:02:51 -0300

    Exercicios de fixação para treinar vetores em Java

---
+++ Vetores/exer_4/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class exer_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numVendedores = 10;
+        int[] quantidades = new int[numVendedores];
+        double[] precos = new double[numVendedores];
+
+        for (int i = 0; i < numVendedores; i++) {
+            System.out.print("Informe a quantidade de peças vendidas pelo vendedor " + (i + 1) + ": ");
+            quantidades[i] = scanner.nextInt();
+
+            System.out.print("Informe o preço da peça vendida pelo vendedor " + (i + 1) + ": ");
+            precos[i] = scanner.nextDouble();
+        }
+
+        double totalVendas = 0;
+
+        for (int i = 0; i < numVendedores; i++) {
+            double valorVenda = quantidades[i] * precos[i];
+            totalVendas += valorVenda;
+
+            System.out.println("Vendedor " + (i + 1) + ":");
+            System.out.println("Quantidade de peças vendidas: " + quantidades[i]);
+            System.out.println("Valor total da venda: " + valorVenda);
+        }
+
+        System.out.println("Quantidade total de peças vendidas por todos os vendedores: " + getTotalQuantidade(quantidades));
+        System.out.println("Valor total das vendas de todos os vendedores: " + totalVendas);
+
+        scanner.close();
+    }
+
+    public static int getTotalQuantidade(int[] quantidades) {
+        int total = 0;
+        for (int quantidade : quantidades) {
+            total += quantidade;
+        }
+        return total;
+    }
+}
